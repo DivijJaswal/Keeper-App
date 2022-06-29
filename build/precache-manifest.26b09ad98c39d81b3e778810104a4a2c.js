9f39953a7a169e3b078366d32bc7015306f6ac1f
DivijJaswal <ellytopas@gmail.com>
2022-06-29 14:17:47 +0530

    first

---
+++ build/precache-manifest.26b09ad98c39d81b3e778810104a4a2c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6aba01b4c825639cb41774a425dca5b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1b017533e6e40d3693f",
+    "url": "/static/js/2.8a02f188.chunk.js"
+  },
+  {
+    "revision": "4f19156d4bee52873dad",
+    "url": "/static/js/main.06660ee6.chunk.js"
+  },
+  {
+    "revision": "31cb9d8849daaa195195",
+    "url": "/static/js/runtime-main.028bd04c.js"
+  }
+]);
